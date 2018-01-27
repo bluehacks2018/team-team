01540daab6f0a6965c5f31a71b0186248e1acf7d
Gab Santiago <gabbysantiago96@yahoo.com>
2018-01-27 22:15:50 +0800

    Added colors to Login activity

--- app/src/main/java/com/teamteam/blueboi/LoginActivity.java
+++ app/src/main/java/com/teamteam/blueboi/LoginActivity.java
@@ -1,13 +1,32 @@
 package com.teamteam.blueboi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button username_login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username_login_button = (Button) findViewById(R.id.username_log_in_button);
+
+        username_login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getBaseContext(), HomeActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
