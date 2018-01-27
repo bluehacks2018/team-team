d3e4a5805084824ede11716ee5e580109c627ee5
DESKTOP-FC16M48\Krizia Lynn <krizia_chiu@dlsu.edu.ph>
2018-01-28 03:47:02 +0800

    Initiate objects

--- app/src/main/java/com/teamteam/blueboi/Request.java
+++ app/src/main/java/com/teamteam/blueboi/Request.java
@@ -15,4 +15,72 @@ public class Request {
     private ArrayList<String> tags;
     private Worker worker;
 
+    public Request(String title, String description, String startDate, String endDate, ArrayList<String> tags, Worker worker) {
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.tags = tags;
+        this.worker = worker;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public Worker getWorker() {
+        return worker;
+    }
+
+    public void setWorker(Worker worker) {
+        this.worker = worker;
+    }
+
+    public Request() {
+
+    }
 }
