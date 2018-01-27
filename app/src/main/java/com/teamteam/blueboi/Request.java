2ff84c6692274b6c309ae669535d5fa8686288a3
DESKTOP-FC16M48\Krizia Lynn <krizia_chiu@dlsu.edu.ph>
2018-01-28 03:45:19 +0800

    Initiate objects

---
+++ app/src/main/java/com/teamteam/blueboi/Request.java
@@ -0,0 +1,18 @@
+package com.teamteam.blueboi;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Krizia Lynn on 28/01/2018.
+ */
+
+public class Request {
+    private String uid;
+    private String title;
+    private String description;
+    private String startDate;
+    private String endDate;
+    private ArrayList<String> tags;
+    private Worker worker;
+
+}
