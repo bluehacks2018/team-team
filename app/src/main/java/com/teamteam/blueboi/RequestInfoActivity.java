12b73d25252a68c13672f0ff4681874969de15d2
Ayano1122 <julianne_kitty@yahoo.com>
2018-01-28 10:31:28 +0800

    random

--- app/src/main/java/com/teamteam/blueboi/RequestInfoActivity.java
+++ app/src/main/java/com/teamteam/blueboi/RequestInfoActivity.java
@@ -1,7 +1,9 @@
 package com.teamteam.blueboi;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RequestInfoActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class RequestInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_info);
+
+        final ActionBar actionBar = getActionBar();
+
+        // actionBar
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+
+        // titleTextView
+        TextView titleTextView = new TextView(actionBar.getThemedContext());
+
+        titleTextView.setText("Title");
+
+        // Add titleTextView into ActionBar
+        actionBar.setCustomView(titleTextView);
     }
 }
